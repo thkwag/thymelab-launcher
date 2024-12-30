e843a897e3dea590959b94a88f7ccf80ccc3ffee
Taehwan Kwag <thkwag@gmail.com>
2024-12-30 12:34:35 +0900

    Add LocaleManager mock and update tests in AppProcessManagerTest

--- src/test/java/com/github/thkwag/thymelab/launcher/process/AppProcessManagerTest.java
+++ src/test/java/com/github/thkwag/thymelab/launcher/process/AppProcessManagerTest.java
@@ -1,6 +1,7 @@
 package com.github.thkwag.thymelab.launcher.process;
 
 import com.github.thkwag.thymelab.launcher.config.ConfigManager;
+import com.github.thkwag.thymelab.launcher.config.LocaleManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.nio.file.Path;
+import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +23,12 @@ class AppProcessManagerTest {
     
     @Mock
     private LogConsumer logConsumer;
+    
+    @Mock
+    private LocaleManager localeManager;
+    
+    @Mock
+    private ResourceBundle resourceBundle;
     
     private AppProcessManager processManager;
     private AtomicBoolean processExited;
@@ -38,12 +46,19 @@ class AppProcessManagerTest {
             configManager
         );
         
+        // Mock basic configuration
         when(configManager.getVersion()).thenReturn("1.0.0");
         when(configManager.getProperty("log.level", "INFO")).thenReturn("INFO");
         when(configManager.getInt("server.port", 8080)).thenReturn(8080);
         when(configManager.getProperty("static.folder.path", "")).thenReturn("");
         when(configManager.getProperty("templates.folder.path", "")).thenReturn("");
         when(configManager.getProperty("data.folder.path", "")).thenReturn("");
+        
+        // Mock processor jar path and locale manager
+        when(configManager.getProcessorJarPath()).thenReturn("");
+        when(configManager.getLocaleManager()).thenReturn(localeManager);
+        when(localeManager.getBundle()).thenReturn(resourceBundle);
+        when(resourceBundle.getString("jar_not_configured")).thenReturn("No JAR configured");
     }
 
     @Test
@@ -55,14 +70,13 @@ class AppProcessManagerTest {
     @Test
     @DisplayName("Test configuration loading")
     void testConfigurationLoading() {
-        // Given
-        when(configManager.getVersion()).thenReturn("1.0.0");
-        
         // When
         processManager.startProcess();
         
         // Then
-        verify(configManager, atLeastOnce()).getVersion();
+        verify(configManager, atLeastOnce()).getProcessorJarPath();
+        verify(configManager, atLeastOnce()).getLocaleManager();
+        verify(logConsumer, atLeastOnce()).accept(anyString());
     }
 
     @Test
@@ -75,6 +89,7 @@ class AppProcessManagerTest {
     @Test
     @DisplayName("Test log consumer")
     void testLogConsumer() {
-        verify(logConsumer, never()).accept(anyString());
+        processManager.startProcess();
+        verify(logConsumer, atLeastOnce()).accept(anyString());
     }
 } 
